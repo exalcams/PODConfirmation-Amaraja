d903803b121d5a02a230d38ffb19fc836b784f7e
exalcams <exalca.plant1@gmail.com>
2020-09-01 15:46:21 +0530

    organization related changes..

--- src/app/models/invoice-details.ts
+++ src/app/models/invoice-details.ts
@@ -127,6 +127,7 @@ export class DeliveryCount {
 export class InvoiceStatusCount {
     TotalInvoices: number;
     ConfirmedInvoices: number;
+    PartiallyConfirmedInvoices: number;
     PendingInvoices: number;
 }
 
